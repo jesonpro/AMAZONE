d1e53213b32ef6b8b597e735ff4b65eea671b802
jesonpro <88507137+jesonpro@users.noreply.github.com>
2021-09-02 01:14:12 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -2,7 +2,7 @@ let WhatsAlexa = require('../events');
 let fs = require('fs');
 let {MessageType} = require('@adiwajshing/baileys');
 let Language = require('../language');
-let Config = require( ../config );
+let Config = require('../config');
 let Lang = Language.getString('tagall');
 let SLang = Language.getString('scrapers');
 
